25d182160a2a9c89d1029370533aff5aefd4efa2
nitendra7 <nitendra8077@gmail.com>
2026-01-19 15:18:22 +0530

    API for multiple students

--- day2part2/src/main/java/com/example/day2part2/controller/Home.java
+++ day2part2/src/main/java/com/example/day2part2/controller/Home.java
@@ -4,11 +4,19 @@ import com.example.day2part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class Home {
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student=new StudentModel(1,"nish","abc@xyz.com");
-        return student;
+    public ArrayList<StudentModel> getStudents(){
+        ArrayList<StudentModel> students=new ArrayList<>();
+        students.add(new StudentModel(1,"nish","abc1@xyz.com"));
+        students.add(new StudentModel(2,"chitrang","abc2@xyz.com"));
+        students.add(new StudentModel(3,"nitendra","abc3@xyz.com"));
+        students.add(new StudentModel(4,"dev","abc4@xyz.com"));
+        students.add(new StudentModel(5,"joginder","abc5@xyz.com"));
+
+        return students;
     }
 }
