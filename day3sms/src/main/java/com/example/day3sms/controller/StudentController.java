e9f7ba60b7b8a31db8d5b3ef8512c28628f4e1e1
nitendra7 <nitendra8077@gmail.com>
2026-01-20 15:20:54 +0530

    MongoDB connection with flow

--- day3sms/src/main/java/com/example/day3sms/controller/StudentController.java
+++ day3sms/src/main/java/com/example/day3sms/controller/StudentController.java
@@ -1,12 +1,22 @@
 package com.example.day3sms.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.day3sms.model.StudentModel;
+import com.example.day3sms.service.StudentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
-    @GetMapping
-    public String hello(){
-        return "Hello";
+
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/add")
+    public StudentModel addStudent(@RequestBody StudentModel student) {
+        return service.add(student);
     }
 }
---
+++ day3sms/src/main/java/com/example/day3sms/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.example.day3sms.service;
+
+import com.example.day3sms.model.StudentModel;
+import com.example.day3sms.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository repo;
+
+    public StudentService(StudentRepository repo) {
+        this.repo = repo;
+    }
+    public StudentModel add(StudentModel student) {
+        return repo.save(student);
+    }
+}
