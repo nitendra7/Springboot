2d2087f8674459138df30da36283abb1e23ed89f
nitendra7 <nitendra8077@gmail.com>
2026-01-27 15:21:00 +0530

    CRUD complete

--- day3sms/src/main/java/com/example/day3sms/controller/StudentController.java
+++ day3sms/src/main/java/com/example/day3sms/controller/StudentController.java
@@ -5,6 +5,12 @@ import com.example.day3sms.service.StudentService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -18,5 +24,20 @@ public class StudentController {
     @PostMapping("/add")
     public StudentModel addStudent(@RequestBody StudentModel student) {
         return service.add(student);
+    }
+
+    @GetMapping("/students")
+    public List<StudentModel> getAllStudents() {
+        return service.getAll();
+    }
+
+    @PutMapping("/students/{id}")
+    public StudentModel updateStudent(@PathVariable String id, @RequestBody StudentModel student) {
+        return service.update(id, student);
+    }
+
+    @DeleteMapping("/students/{id}")
+    public String deleteStudent(@PathVariable String id) {
+        return service.delete(id);
     }
 }
--- day3sms/src/main/java/com/example/day3sms/service/StudentService.java
+++ day3sms/src/main/java/com/example/day3sms/service/StudentService.java
@@ -4,6 +4,8 @@ import com.example.day3sms.model.StudentModel;
 import com.example.day3sms.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentService {
 
@@ -12,7 +14,22 @@ public class StudentService {
     public StudentService(StudentRepository repo) {
         this.repo = repo;
     }
+
     public StudentModel add(StudentModel student) {
         return repo.save(student);
+    }
+
+    public List<StudentModel> getAll() {
+        return repo.findAll();
+    }
+
+    public StudentModel update(String id, StudentModel student) {
+        student.setId(id);
+        return repo.save(student);
+    }
+
+    public String delete(String id) {
+        repo.deleteById(id);
+        return "Student deleted successfully.";
     }
 }
